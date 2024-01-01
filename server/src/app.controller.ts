c8a03afc55e863b78e83f12bf5b8c8a13d567c91
Igorko Gorgonzola <30935475+DonAlexandro@users.noreply.github.com>
2024-01-01 23:47:41 +0200

    feat: upload file to the backend

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
     return this.appService.getHello();
   }
 }
