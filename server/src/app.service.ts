edddba7f05db3f967d4bb874e8943fa1950b0453
Igorko Gorgonzola <30935475+DonAlexandro@users.noreply.github.com>
2024-01-04 23:44:32 +0200

    feat: add image recognition

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import admin from 'firebase-admin';
 import { getDownloadURL } from 'firebase-admin/storage';
+import vision from '@google-cloud/vision';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
-  async uploadToExternalStorage(fileName: string, file: Buffer) {
+  private visionClient = new vision.ImageAnnotatorClient({
+    keyFilename: path.resolve(path.join(__dirname, '../admin-account.json')),
+  });
+
+  private async uploadToExternalStorage(fileName: string, file: Buffer) {
     const bucket = admin.storage().bucket();
     const storageFile = bucket.file(fileName);
 
@@ -12,6 +18,20 @@ export class AppService {
 
     const imageUrl = await getDownloadURL(storageFile);
 
-    return { imageUrl };
+    return imageUrl;
+  }
+
+  private async analyze(imageUrl: string) {
+    const [result] = await this.visionClient.objectLocalization(imageUrl);
+
+    return result.localizedObjectAnnotations[0].name;
+  }
+
+  async handleImage(fileName: string, file: Buffer) {
+    const imageUrl = await this.uploadToExternalStorage(fileName, file);
+
+    const objectOnImage = await this.analyze(imageUrl);
+
+    return { result: objectOnImage };
   }
 }
