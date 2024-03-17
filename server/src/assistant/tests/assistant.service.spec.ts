6b4ba9fc2cbf70351bdbf2facd39f11ce8e9bc83
Bartosz Pierdolny <parashnokwtf@gmail.com>
2024-03-17 11:37:21 +0200

    feat: cover assistant module in tests

--- server/src/assistant/tests/assistant.service.spec.ts
+++ server/src/assistant/tests/assistant.service.spec.ts
@@ -1,8 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssistantService } from '../assistant.service';
+import { InternalServerErrorException } from '@nestjs/common';
+import { ASSISTANT_GENERIC_ERROR } from '../utils';
 
 describe('AssistantService', () => {
   let service: AssistantService;
+
+  const mockGenerativeModel = (content?: { parts: { text: string }[] }) => {
+    Reflect.set(service, 'generativeModel', {
+      generateContent: jest.fn().mockResolvedValue({
+        response: {
+          candidates: [
+            {
+              content,
+            },
+          ],
+        },
+      }),
+    });
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +28,25 @@ describe('AssistantService', () => {
     service = module.get<AssistantService>(AssistantService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return a phrase generated based on the given word', async () => {
+    mockGenerativeModel({
+      parts: [{ text: 'An apple a day keeps doctor away.' }],
+    });
+
+    const phrase = await service.generatePhrase('Apple');
+
+    expect(phrase).toBe('An apple a day keeps doctor away.');
+  });
+
+  it('should throw InternalServerErrorException in case phrase was not generated correctly', async () => {
+    mockGenerativeModel();
+
+    expect(async () => {
+      await service.generatePhrase('Apple');
+    }).rejects.toThrow(
+      new InternalServerErrorException(
+        ASSISTANT_GENERIC_ERROR.replace(':word', 'Apple'),
+      ),
+    );
   });
 });
