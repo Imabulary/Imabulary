1246423d2efc90bf583f9deee7f45fb76a59f606
vladyslav <vladyslav-panchenko280>
2024-06-25 22:38:24 +0300

    fix(payment): add user service to wallet module

--- server/src/wallet/wallet.module.ts
+++ server/src/wallet/wallet.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
+import { UsersService } from 'src/users';
 
 @Module({
   controllers: [WalletController],
-  providers: [PrismaService, WalletService],
+  providers: [PrismaService, WalletService, UsersService],
 })
 export class WalletModule {}
