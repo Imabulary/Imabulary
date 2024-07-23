7072a2ae6a25f31bb57306083e721f90d8193325
vladyslav-panchenko280 <vladpanchenko280@gmail.com>
2024-07-23 22:52:42 +0300

    IMB-26: doc. swagger integration

--- server/openapi/swagger.config.ts
+++ server/openapi/swagger.config.ts
@@ -1,8 +1,15 @@
-import { SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 
 export function setupSwagger(app: INestApplication): void {
-  SwaggerModule.setup('api', app, undefined, {
+  const options = new DocumentBuilder()
+    .setTitle('Imabulary API')
+    .setDescription('API documentation for the Imabulary API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, {
     yamlDocumentUrl: 'swagger.yaml',
   });
 }
