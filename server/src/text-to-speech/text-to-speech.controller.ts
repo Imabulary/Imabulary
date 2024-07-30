63a8eb0f8fd754a8c8061d5cb5aa56c906ab26b2
vladyslav-panchenko280 <vladpanchenko280@gmail.com>
2024-07-30 18:08:49 +0300

    IMB-68: feat. text-to-speech

--- server/src/text-to-speech/text-to-speech.controller.ts
+++ server/src/text-to-speech/text-to-speech.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { TextToSpeechService } from './text-to-speech.service';
+import { Response } from 'express';
 
-@Controller('tts')
+@Controller('text-to-speech')
 export class TextToSpeechController {
   constructor(private readonly textToSpeechService: TextToSpeechService) {}
 
   @Post('synthesize')
   async synthesizeSpeech(
     @Body('text') text: string,
-  ): Promise<{ audioContent: string }> {
-    const audioContent = await this.textToSpeechService.synthesizeSpeech(text);
-    return { audioContent };
+    @Res() res: Response,
+  ): Promise<void> {
+    const base64Audio = await this.textToSpeechService.synthesizeSpeech(text);
+    const audioBuffer = Buffer.from(base64Audio, 'base64');
+
+    res.setHeader('Content-Type', 'audio/mpeg');
+    res.setHeader('Content-Length', audioBuffer.length.toString());
+
+    res.end(audioBuffer);
   }
 }
