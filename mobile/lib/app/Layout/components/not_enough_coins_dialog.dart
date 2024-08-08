e5df367a906a907bdad10fa5a4183fa8bc00b76d
Bartosz Pierdolny <parashnokwtf@gmail.com>
2024-08-08 15:31:11 +0300

    feat: add popup saying that you don't have enough coins

---
+++ mobile/lib/app/Layout/components/not_enough_coins_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/app/Wallet/presentation/wallet_screen.dart';
+import 'package:mobile/atoms/colors.dart';
+import 'package:mobile/atoms/type_setting.dart';
+
+class NotEnoughCoinsDialog extends StatelessWidget {
+  const NotEnoughCoinsDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const TypeSetting(
+        'Oops!',
+        variant: TextVariants.headlineLarge,
+      ),
+      content: const TypeSetting(
+        "You don't have enough coins to take new photo",
+      ),
+      actions: [
+        TextButton(
+          child: const TypeSetting(
+            'Cancel',
+            style: TextStyle(color: AppColors.primary),
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        ElevatedButton(
+          child: const TypeSetting(
+            'Buy Coins',
+            style: TextStyle(color: AppColors.primary),
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const WalletScreen(),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
