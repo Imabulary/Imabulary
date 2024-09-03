eeab801c258eb33acc5c090b038a9a8f4721ffd8
vladyslav-panchenko280 <vladpanchenko280@gmail.com>
2024-09-03 18:07:26 +0300

    IMB-189: validate name and description length for set

--- mobile/lib/app/Set/domain/set.dart
+++ mobile/lib/app/Set/domain/set.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-
+import 'package:mobile/shared/constants.dart';
 part 'set.freezed.dart';
 part 'set.g.dart';
 
@@ -11,8 +11,8 @@ typedef SetsFlashcards = ({String image_url});
 class Set with _$Set {
   const factory Set({
     required String id,
-    required String name,
-    String? description,
+    @NameValidation()  String name,
+    @DescriptionValidation() String? description,
     required String userId,
     required DateTime createdAt,
     required DateTime updatedAt,
@@ -20,4 +20,34 @@ class Set with _$Set {
   }) = _Set;
 
   factory Set.fromJson(Map<String, dynamic> json) => _$SetFromJson(json);
+}
+
+class NameValidation implements JsonConverter<String, String> {
+  const NameValidation();
+
+  @override
+  String fromJson(String json) {
+    if (json.length > maxSetNameLength) {
+      throw ArgumentError('Name cannot be longer than ${maxSetNameLength} characters');
+    }
+    return json;
+  }
+
+  @override
+  String toJson(String object) => object;
+}
+
+class DescriptionValidation implements JsonConverter<String?, String?> {
+  const DescriptionValidation();
+
+  @override
+  String? fromJson(String? json) {
+    if (json != null && json.length > maxSetDescriptionLength) {
+      throw ArgumentError('Description cannot be longer than ${maxSetDescriptionLength} characters');
+    }
+    return json;
+  }
+
+  @override
+  String? toJson(String? object) => object;
 }
--- mobile/lib/shared/constants.dart
+++ mobile/lib/shared/constants.dart
@@ -1,1 +1,4 @@
 const kStubImageUrl = 'https://placehold.co/110@2x.png?text=No+Picture';
+
+const maxSetNameLength = 50;
+const maxSetDescriptionLength = 200;
