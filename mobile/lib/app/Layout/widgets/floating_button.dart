8e1468b0f29ca7588a2d023f82b6f618de26f816
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-11-04 22:22:34 +0200

    IMB-336: Make navigation to the root from a flashcard

--- mobile/lib/app/Layout/widgets/floating_button.dart
+++ mobile/lib/app/Layout/widgets/floating_button.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/atoms/colors.dart';
-import 'AddBottomSheet/add_bottom_sheet.dart';
 
 class FloatingButton extends StatelessWidget {
   const FloatingButton({super.key, required this.onPressed});
 
   final VoidCallback onPressed;
-  
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
