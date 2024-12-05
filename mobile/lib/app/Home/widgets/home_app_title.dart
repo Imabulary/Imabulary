c5dd6258e51c068232c08c1aa127dbc593a03ea5
Bartosz Pierdolny <parashnokwtf@gmail.com>
2024-12-05 15:21:36 +0200

    fix: remove commented code

--- mobile/lib/app/Home/widgets/home_app_title.dart
+++ mobile/lib/app/Home/widgets/home_app_title.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile/app/Flashcard/domain/card/card.dart';
 import 'package:mobile/app/Home/components/welcome_title.dart';
-import 'package:mobile/app/Wallet/application/wallet_providers.dart';
-import 'package:mobile/app/Wallet/components/balance.dart';
-import 'package:mobile/atoms/type_setting.dart';
 import 'package:mobile/shared/models/ServerResponse/server_response.dart';
 
 class HomeAppTitle extends ConsumerWidget {
@@ -17,8 +14,6 @@ class HomeAppTitle extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final wallet = ref.watch(getWalletBalanceProvider);
-
     final welcomeTitle =
         flashcards.hasValue && flashcards.value!.result.isNotEmpty
             ? const WelcomeTitle(customTitle: "Welcome back 👋")
@@ -28,12 +23,6 @@ class HomeAppTitle extends ConsumerWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         welcomeTitle,
-        // const SizedBox(width: 8),
-        // wallet.when(
-        //   data: (data) => Balance(data.balance),
-        //   error: (error, _) => const Balance(0),
-        //   loading: () => const Balance(0),
-        // )
       ],
     );
   }
