c60a30c22a818675b55e6312e10201227f7dcac3
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2025-02-12 13:53:11 +0200

    IMB-451: Add visual sound feedback when user taps on speaker icon

--- mobile/lib/app/Flashcard/components/voiceover_button.dart
+++ mobile/lib/app/Flashcard/components/voiceover_button.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/app/Flashcard/components/voiceover_button_controller.dart';
 
-class VoiceoverButton extends StatelessWidget {
+class VoiceoverButton extends StatefulWidget {
   const VoiceoverButton({super.key, required this.audioUrl});
 
   final String audioUrl;
 
   @override
+  State<VoiceoverButton> createState() => _VoiceoverButtonState();
+}
+
+class _VoiceoverButtonState extends State<VoiceoverButton> {
+  bool isPlaying = false;
+
+  @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        VoiceoverButtonController.play(audioUrl);
+      onPressed: () async {
+        if (!isPlaying) {
+          setState(() => isPlaying = true);
+          await VoiceoverButtonController.play(widget.audioUrl);
+          setState(() => isPlaying = false);
+        }
       },
-      icon: const Icon(Icons.volume_up),
+      icon: Icon(
+        isPlaying ? Icons.volume_up_outlined : Icons.volume_off_outlined,
+      ),
     );
   }
 }
